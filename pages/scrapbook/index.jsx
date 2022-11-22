c8bba264918dd25aa1510bb260811ad1b08a14f4
Nicholas <contactnicholas81@gmail.com>
2022-11-22 10:55:39 +0000

    refactor: fix eslint errors and cleanup code

--- pages/scrapbook/index.jsx
+++ pages/scrapbook/index.jsx
@@ -1,79 +1,132 @@
-import { useSupabaseClient } from '@supabase/auth-helpers-react'
-import { useEffect, useState } from 'react'
-
-import Image from 'next/image'
-import { format } from 'date-fns' 
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import dayjs from 'dayjs';
 
 export default function Scrapbook() {
-    const supabase = useSupabaseClient()
+  const supabase = useSupabaseClient();
 
-    let [posts, setPosts] = useState()
-    let [loading, setLoading] = useState(true)
+  let [loading, setLoading] = useState(true);
+  let [posts, setPosts] = useState();
 
-    useEffect(() => {
-        fetchPosts()
-    }, [])
+  useEffect(() => {
+    fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    async function fetchPosts() {
-        setLoading(true)
-        
-        let { data, error, status } = await supabase
-            .from('scrapbook')
-            .select()
-            .order('created_at', { ascending: false })
-        
-        console.log('data ran', data, error, status)
-        
-        if (error) return alert('An error occured');
+  async function fetchPosts() {
+    setLoading(true);
 
-        setPosts(data)
-        setLoading(false)
-    }
+    let { data, error, status } = await supabase
+      .from('scrapbook')
+      .select()
+      .order('created_at', { ascending: false });
 
-    return (
-        <main className="max-w-7xl mx-auto my-16">
-            {loading && <p>Loading...</p>}
+    console.log('data ran', data, error, status);
 
-            <section className="columns-1 md:columns-4 gap-4 space-y-4">
-                {posts && posts.map(({ id, author, created_at, content, media, message_id, author_avatar, author_name }) => (
-                    <article
-                        key={id}
-                        className="rounded-lg bg-neutral-800 relative shadow-md shadow-black/20"
-                    >
-                        {media && media.map(({ url, type, height, size, width}) => (
-                            <div key={url}>
-                                {type === 'video/mp4' ? (
-                                    <video className="w-full rounded-t-lg object-cover" controls>
-                                        <source src={url} type={type} />
-                                    </video>
-                                ) : (
-                                    <img src={url} alt="Scrapbook Image" className="w-full rounded-t-lg object-cover" />
-                                )}
-                            </div>
-                        ))}
+    if (error) return alert('An error occurred');
 
-                        {media.length > 0 && (
-                            <a href={`/scrapbook/user/${author}`} className="flex space-x-2 items-center absolute top-2 left-2 border border-neutral-900/10 bg-neutral-900/50 backdrop-blur py-1 pl-1 pr-2 rounded-full">
-                                    <img src={author_avatar} className="h-6 w-6 aspect-square rounded-full" />
-                                    <span className="text-sm">{author_name}</span>
-                            </a>
-                        )}
+    setPosts(data);
+    setLoading(false);
+  }
 
-                        <div className="space-y-4 p-4">
-                            {media.length < 1 && (
-                                <a href={`/scrapbook/user/${author}`} className="flex space-x-2 items-center">
-                                        <img src={author_avatar} className="h-6 w-6 aspect-square rounded-full" />
-                                        <span className="">{author_name}</span>
-                                </a>
-                            )}
-
-                            <p className="">{content}</p>
-
-                            {format(new Date(created_at), 'MMM do')}
+  return (
+    <main className="mx-auto my-16 max-w-7xl p-10">
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h1 className="mb-6 text-4xl font-bold tracking-tight">Scrapbook</h1>
+          <section className="columns-4 gap-4 space-y-4 md:columns-4">
+            {posts &&
+              posts.map(
+                ({
+                  id,
+                  author,
+                  created_at,
+                  content,
+                  media,
+                  message_id,
+                  author_avatar,
+                  author_name,
+                }) => (
+                  <article
+                    key={id}
+                    className="relative rounded-lg bg-neutral-800 shadow-md shadow-black/20"
+                  >
+                    {media &&
+                      media.map(({ url, type, height, size, width }) => (
+                        <div key={url}>
+                          {type === 'video/mp4' ? (
+                            <video
+                              className="w-full rounded-t-lg object-cover"
+                              controls
+                            >
+                              <source src={url} type={type} />
+                            </video>
+                          ) : (
+                            <picture>
+                              <img
+                                src={url}
+                                alt="Scrapbook Image"
+                                className="w-full rounded-t-lg object-cover"
+                              />
+                            </picture>
+                          )}
                         </div>
-                    </article>
-                ))}
-            </section>
-        </main>
-    );
+                      ))}
+
+                    {media.length > 0 && (
+                      <Link
+                        href={`/scrapbook/user/${author}`}
+                        className="absolute top-2 left-2 flex items-center space-x-2 rounded-full border border-neutral-900/10 bg-neutral-900/50 py-1 pl-1 pr-2 backdrop-blur"
+                      >
+                        <picture>
+                          <img
+                            src={author_avatar}
+                            className="aspect-square h-6 w-6 rounded-full"
+                            alt="Scrapbook Author"
+                          />
+                        </picture>
+                        <span className="text-sm font-semibold">
+                          {author_name}
+                        </span>
+                      </Link>
+                    )}
+
+                    <div className="space-y-4 p-4">
+                      {media.length < 1 && (
+                        <Link
+                          href={`/scrapbook/user/${author}`}
+                          className="flex items-center space-x-2"
+                        >
+                          <picture>
+                            <img
+                              src={author_avatar}
+                              className="aspect-square h-6 w-6 rounded-full"
+                              alt="Scrapbook Author Avatar"
+                            />
+                          </picture>
+                          <span className="">{author_name}</span>
+                        </Link>
+                      )}
+
+                      <div>
+                        {content ? (
+                          <p className="mb-3 text-gray-100">{content}</p>
+                        ) : null}
+                      </div>
+
+                      <time className="text-sm text-gray-300">
+                        {dayjs(created_at.toString()).format('MMMM D, YYYY')}
+                      </time>
+                    </div>
+                  </article>
+                )
+              )}
+          </section>
+        </>
+      )}
+    </main>
+  );
 }
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import("prettier").Config} */
 module.exports = {
-    plugins: [require.resolve('prettier-plugin-tailwindcss')],
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-}
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
