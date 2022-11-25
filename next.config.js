24b9a4fd2947fb37c07358cbc5011c2850a9b955
kttykat <kat@saige.lgbt>
2022-11-25 22:53:04 +0100

    Added placeholder landing

--- next.config.js
+++ next.config.js
@@ -5,6 +5,30 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    redirects: async () => {
+        return [
+            {
+                destination: 'https://github.com/discordfrens',
+                source: '/github',
+                permanent: false,
+            },
+            {
+                destination: 'https://twitter.com/frensofdiscord',
+                source: '/twitter',
+                permanent: false,
+            },
+            {
+                destination: 'https://discord.gg/a9AmhF3YQM',
+                source: '/discord',
+                permanent: false,
+            },
+            {
+                destination: 'https://learn.discordfrens.com',
+                source: '/learn',
+                permanent: false,
+            },
+        ];
+    },
 };
 
 module.exports = nextConfig;
